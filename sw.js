15ba7b87c40fa7244142a9c22d211abcfe36dc58
GitHub Action <kejiweixun@gmail.com>
2020-08-14 04:44:44 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,13 +27,13 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-19a65087c9d2261aeafb.js"
+    "url": "webpack-runtime-cd0a8c3769dc2f45e29a.js"
   },
   {
     "url": "framework-a38040c4749cf445737f.js"
   },
   {
-    "url": "styles.3dd06fdc38e97ac2c319.css"
+    "url": "styles.4dee7924fb57931b7823.css"
   },
   {
     "url": "styles-7573517523cb2796c77b.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1125338c6bb37731b84bb744c3627aba"
+    "revision": "39bc2be8dd601bd9bef9003a6dbc3034"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b81c43cf3b4ef9854106.js"
